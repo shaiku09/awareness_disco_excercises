faa09f6d42a9863cd114c116661ff21c8a01e754
Shaikh <Ubair.Shaikh@ons.gov.uk>
2024-12-11 15:20:58 +0000

    added disco helper functions and completed some exercises

---
+++ Disco_helper_functions.py
@@ -0,0 +1,6 @@
+def change_age(df_in):
+    df_out = df_in.copy()
+    widowed_25a = df_out[(df_out['Age']<=25) & (df_out['Marital Status']=='Widowed')]
+    df_out.loc[widowed_25a.index, 'Age'] = 25
+    return df_out
+
